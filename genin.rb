97e609d5548f8632fcdb7e0c674488e10be7039f
dmitry.travyan <dmitry.travyan@picodata.io>
2022-11-10 10:59:01 +0300

    feat: up genin version

--- genin.rb
+++ genin.rb
@@ -13,3 +13,37 @@ class Genin < Formula
     system "#{bin}/genin"
   end
 end
+
+class Genin < Formula
+  desc "Quick inventory generation for tarantool apps"
+  homepage "https://github.com/picodata/genin"
+  url "https://binary.picodata.io/repository/raw/genin/osx/genin-0.4.0-x86_64-macosx.tar.gz"
+  sha256 "1171e809a891a68e53645b8774a8aa09248969485934fcb62ff8a85d8f7c7b89"
+  version "0.4.0"
+  license "BSD-2-Clause license"
+
+  def install
+    bin.install "genin"
+  end
+
+  test do
+    system "#{bin}/genin"
+  end
+end
+
+class Genin < Formula
+  desc "Quick inventory generation for tarantool apps"
+  homepage "https://github.com/picodata/genin"
+  url "https://binary.picodata.io/repository/raw/genin/osx/genin-0.4.1-x86_64-macosx.tar.gz"
+  sha256 "15ac581d3f658b63b4d746775edfb0777e6867e75b0b08c89e21800695ba12a9"
+  version "0.4.1"
+  license "BSD-2-Clause license"
+
+  def install
+    bin.install "genin"
+  end
+
+  test do
+    system "#{bin}/genin"
+  end
+end
