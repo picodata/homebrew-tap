c1c838477684bffaa0c843021d45d50cc8782a96
dmitry.travyan <dmitry.travyan@picodata.io>
2022-11-15 19:48:21 +0300

    fix: genin 0.4.1 bigfix

--- Formula/genin.rb
+++ Formula/genin.rb
@@ -2,7 +2,7 @@ class Genin < Formula
   desc "Quick inventory generation for tarantool apps"
   homepage "https://github.com/picodata/genin"
   url "https://binary.picodata.io/repository/raw/genin/osx/genin-0.4.1-x86_64-macosx.tar.gz"
-  sha256 "466568240fc7c079636dc68f65070ed66e3c3e9196940e052ad5cf67a9a7cb2a"
+  sha256 "28f82c4a4f27a9b24fc45e3c67ab01028b3a3f94bf47d0bf6708a7c86ab9527e"
   version "0.4.1"
   license "BSD-2-Clause license"
 
