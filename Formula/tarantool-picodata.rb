08f0a46a99ee14757a2f84dc4485de9f8f8e720a
Dmitry Kibirev <zx85@list.ru>
2023-04-06 17:27:09 +0300

    update bottle package

--- Formula/tarantool-picodata.rb
+++ Formula/tarantool-picodata.rb
@@ -14,9 +14,9 @@ class TarantoolPicodata < Formula
 
   bottle do
     root_url "https://binary.picodata.io/repository/raw/brew/bottle"
-    sha256 cellar: :any,  arm64_ventura:    "49eecfca4e5580b84d3f7410b51fadab0c21243784fd8647a7e24f1da8c5a62b"
-    sha256 cellar: :any,  arm64_monterey:   "49eecfca4e5580b84d3f7410b51fadab0c21243784fd8647a7e24f1da8c5a62b"
-    sha256 cellar: :any,  arm64_big_sur:    "49eecfca4e5580b84d3f7410b51fadab0c21243784fd8647a7e24f1da8c5a62b"
+    sha256 cellar: :any,  arm64_ventura:    "73324afa0f183b33133f6f76aefb5799ca94f465087be8e6f9cdbb59299300fc"
+    sha256 cellar: :any,  arm64_monterey:   "73324afa0f183b33133f6f76aefb5799ca94f465087be8e6f9cdbb59299300fc"
+    sha256 cellar: :any,  arm64_big_sur:    "73324afa0f183b33133f6f76aefb5799ca94f465087be8e6f9cdbb59299300fc"
   end
 
   depends_on "cmake" => :build
