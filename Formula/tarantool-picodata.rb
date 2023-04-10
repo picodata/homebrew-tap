cda975baa49e840353fd9d474c5af23f8b7f5e56
dmitry.travyan <dmitry.travyan@picodata.io>
2023-04-10 11:45:46 +0300

    fix: tarantool-picodata checksum

--- Formula/tarantool-picodata.rb
+++ Formula/tarantool-picodata.rb
@@ -1,8 +1,8 @@
 class TarantoolPicodata < Formula
   desc "Picodata fork tarantool in-memory database and Lua application server"
   homepage "https://picodata.io/"
-  url "https://binary.picodata.io/repository/raw/brew/packages/tarantool-picodata-2.10.4.14.tar.gz"
-  sha256 "eb2bd7b175fefd7f10ee02d525afc02ee3ecb1eef2b7f7a95e02d5711471606c"
+  url "https://binary.picodata.io/repository/raw/brew/packages/tarantool-picodata-2.10.4.tar.gz"
+  sha256 "375cdaa3787ceb819d1f6039097ddb1ea58060ab6c1f6b8b83ec93cb175500f7"
   license "BSD-2-Clause"
   version_scheme 1
   head "https://git.picodata.io/picodata/tarantool.git", branch: "2.10.4-picodata"
