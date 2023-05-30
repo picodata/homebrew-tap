ec810197e7aaa5df8a1495262fd4b156873d6d08
dmitry.travyan <dmitry.travyan@picodata.io>
2023-05-30 11:35:54 +0300

    feat: v0.4.19

--- Formula/genin.rb
+++ Formula/genin.rb
@@ -1,9 +1,9 @@
 class Genin < Formula
   desc "Quick inventory generation for tarantool apps"
   homepage "https://github.com/picodata/genin"
-  url "https://binary.picodata.io/repository/raw/genin/osx/genin-0.4.18-x86_64-macosx.tar.gz"
-  sha256 "6d11f9fd500987ba03627b843b29ea0d1a5496c00d5e3b074bc659378b93417e"
-  version "0.4.18"
+  url "https://binary.picodata.io/repository/raw/genin/osx/genin-0.4.19-x86_64-macosx.tar.gz"
+  sha256 "d85f4c5f94fe6df48e2b4951d86a8dac30c28f1d70502ea5a6ac1510e81694d2"
+  version "0.4.19"
   license "BSD-2-Clause license"
 
   def install
