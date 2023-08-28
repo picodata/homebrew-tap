7a40bdadf519c5bc466ab8becf027ec27189af35
Fedor Telnov <f.telnov@picodata.io>
2023-08-28 14:28:54 +0300

    fix(genin): correct sha256 sum of 0.5.5 release

--- Formula/genin@0.5.5.rb
+++ Formula/genin@0.5.5.rb
@@ -2,7 +2,7 @@ class GeninAT055 < Formula
   desc "Quick inventory generation for tarantool apps"
   homepage "https://github.com/picodata/genin"
   url "https://binary.picodata.io/repository/raw/genin/osx/genin-0.5.5-x86_64-macosx.tar.gz"
-  sha256 "37b7fa8a98a4cfb83432efe0a11be8ce918eb409207df4a88ab611a9c10d1138"
+  sha256 "c050e7fda47b2788327bd1baa28e0cadc699a7f90f952bd3f4d3d296c9221a1f"
   version "0.5.5"
   license "BSD-2-Clause license"
 
