f4a80456e3d954399bfae182bef8ac490435b301
Fedor Telnov <f.telnov@picodata.io>
2023-10-18 11:02:31 +0300

    genin: fix hashsum of 0.5.7 release

--- Formula/genin@0.5.7.rb
+++ Formula/genin@0.5.7.rb
@@ -2,7 +2,7 @@ class GeninAT057 < Formula
   desc "Quick inventory generation for tarantool apps"
   homepage "https://github.com/picodata/genin"
   url "https://binary.picodata.io/repository/raw/genin/osx/genin-0.5.7-x86_64-macosx.tar.gz"
-  sha256 "574e87c14f0f606e80e9137e0d4844eeda1907367625ec55c9ee2e194a002779"
+  sha256 "b295846ef8b4765583327dfc19b3989693d665b209638f06c27c99c7d8b6da55"
   version "0.5.7"
   license "BSD-2-Clause license"
 
