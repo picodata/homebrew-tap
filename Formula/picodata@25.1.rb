6422c2d6fd05084825307b287091c62e49d5fadc
Alexey Protsenko <a.protsenko@picodata.io>
2025-04-03 18:40:18 +0300

    fix sha

--- Formula/picodata@25.1.rb
+++ Formula/picodata@25.1.rb
@@ -2,7 +2,7 @@ class PicodataAT251 < Formula
   desc "Picodata in-memory database and Lua application server"
   homepage "https://picodata.io/"
   url "https://download.picodata.io/tarantool-picodata/macos/picodata-25.1.1.0.tar.gz"
-  sha256 "b67eefbc2129fc9b95e0a6653314bcd4264d49d19030d0862530076da50e2280"
+  sha256 "4e7089d65c8bc9445b0b50e8039498fc38c567b2554a6e351ba0ddf68923fdfb"
   license "BSD-2-Clause"
   version_scheme 1
   head "https://git.picodata.io/core/picodata.git", branch: "25.1.1"
@@ -19,11 +19,11 @@ class PicodataAT251 < Formula
     sha256 cellar: :any,  arm64_big_sur:    "b2f3bf3069424ad41beda14b5d54fcb63002ddd08d2dae4a8cd73195b969caff"
     sha256 cellar: :any,  arm64_sonoma:     "b2f3bf3069424ad41beda14b5d54fcb63002ddd08d2dae4a8cd73195b969caff"
     sha256 cellar: :any,  arm64_sequoia:    "b2f3bf3069424ad41beda14b5d54fcb63002ddd08d2dae4a8cd73195b969caff"
-    sha256 cellar: :any,  ventura:          "5b903dad6d1bfbbdbe4599f76161247bf8258e93a1ef8d585eec8a7ea9c3bdc1"
-    sha256 cellar: :any,  monterey:         "5b903dad6d1bfbbdbe4599f76161247bf8258e93a1ef8d585eec8a7ea9c3bdc1"
-    sha256 cellar: :any,  big_sur:          "5b903dad6d1bfbbdbe4599f76161247bf8258e93a1ef8d585eec8a7ea9c3bdc1"
-    sha256 cellar: :any,  sonoma:           "5b903dad6d1bfbbdbe4599f76161247bf8258e93a1ef8d585eec8a7ea9c3bdc1"
-    sha256 cellar: :any,  sequoia:          "5b903dad6d1bfbbdbe4599f76161247bf8258e93a1ef8d585eec8a7ea9c3bdc1"
+    sha256 cellar: :any,  ventura:          "b67eefbc2129fc9b95e0a6653314bcd4264d49d19030d0862530076da50e2280"
+    sha256 cellar: :any,  monterey:         "b67eefbc2129fc9b95e0a6653314bcd4264d49d19030d0862530076da50e2280"
+    sha256 cellar: :any,  big_sur:          "b67eefbc2129fc9b95e0a6653314bcd4264d49d19030d0862530076da50e2280"
+    sha256 cellar: :any,  sonoma:           "b67eefbc2129fc9b95e0a6653314bcd4264d49d19030d0862530076da50e2280"
+    sha256 cellar: :any,  sequoia:          "b67eefbc2129fc9b95e0a6653314bcd4264d49d19030d0862530076da50e2280"
   end
 
   depends_on "cmake" => :build
